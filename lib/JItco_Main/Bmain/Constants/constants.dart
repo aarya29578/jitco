ed66331a5e809ea76824203e37e66a704be4b921
Aarya Parulekar <aryaparulekar647@gmail.com>
2026-09-08 14:52:27 +0530

    updated urls

--- lib/JItco_Main/Bmain/Constants/constants.dart
+++ lib/JItco_Main/Bmain/Constants/constants.dart
@@ -1,3 +1,4 @@
-const String jitUrl = 'https://api.jitco.in/api/v1';
+//const String jitUrl = 'https://api.jitco.in/api/v1';
 // const String jitUrl = 'https://jitco.salt-tech.com/api/v1';
-//const String jitUrl = "http://89.116.32.117:8001/api/v1"; 
+// const String jitUrl = "http://89.116.32.117:8001/api/v1"; 
+const String jitUrl = "http://192.168.0.106:8000/api/v1"; 
